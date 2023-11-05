50371a72f160a53524d363061c46925799f44949
ryanjenoski <ryanjenoski2005@gmail.com>
2023-11-05 04:50:54 -0500

    mut

--- app/src/main/java/com/example/invasivespecies_hackrpi_android/MapFragment.kt
+++ app/src/main/java/com/example/invasivespecies_hackrpi_android/MapFragment.kt
@@ -1,10 +1,15 @@
 package com.example.invasivespecies_hackrpi_android
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,13 +28,14 @@ class MapFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
 
-    override fun onCreateView(
+    override fun onCreateView (
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
--- app/src/main/java/com/example/invasivespecies_hackrpi_android/MapsActivity.kt
+++ app/src/main/java/com/example/invasivespecies_hackrpi_android/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.invasivespecies_hackrpi_android
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,8 +9,9 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.example.invasivespecies_hackrpi_android.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -39,10 +41,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        val db = FirebaseFirestore.getInstance()
+        val collection = db.collection("hackrpi-csv-1")
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        collection.get().addOnCompleteListener {
+            if (it.isSuccessful) {
+                for (document in it.result) {
+                    val x = document.getString("x")
+                    val y = document.getString("y")
+                    var muthuAssCoordinates = LatLng(0.0, 0.0)
+                    if(x != null && y != null) {
+                        muthuAssCoordinates = LatLng(x.toDouble(), y.toDouble())
+                    }
+                    val circleOptions = CircleOptions()
+                        .center(muthuAssCoordinates)
+                        .radius(500000.0)
+                        .strokeColor(Color.RED) // Set the stroke (border) color
+                        .fillColor(Color.rgb(0, 0, 255)) // Set the fill (inside) color
+
+                    val circle = googleMap.addCircle(circleOptions)
+                }
+            }
+        }
     }
 }
