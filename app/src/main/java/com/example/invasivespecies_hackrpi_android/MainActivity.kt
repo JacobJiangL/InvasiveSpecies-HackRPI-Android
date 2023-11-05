0b505e050e3949f17ee2a2e7f0d5ee7d4934a78b
armaan <armaan_priyadarshan@outlook.com>
2023-11-04 23:22:51 -0400

    muthu

--- app/src/main/java/com/example/invasivespecies_hackrpi_android/MainActivity.kt
+++ app/src/main/java/com/example/invasivespecies_hackrpi_android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.invasivespecies_hackrpi_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.invasivespecies_hackrpi_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +12,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomNavigationView.setOnItemReselectedListener {
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.map_button -> {
+                    loadFragment(MapFragment())
+                    true
+                }
+                R.id.camera_button -> {
+                    loadFragment(CameraFragment())
+                    true
+                }
 
+                else -> {
+                    true
+                }
+            }
         }
+    }
+    private fun loadFragment(frag: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout, frag)
+        transaction.commit()
     }
 }
